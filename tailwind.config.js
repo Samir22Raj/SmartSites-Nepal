22ca84b2c8206573f624cb89effa444380b5b82b
Samir Rajkarnikar <samirrajkarnikar9@gmail.com>
2024-03-04 14:11:55 +0545

    featured section complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ module.exports = {
 			colors: {
 				"black-new": "#0F0A09",
 				yellow: "#F1BC1E",
+				card: "#FFFFFF4D",
+				"off-white": "#F5F5F5",
 			},
 			backgroundImage: {
 				services: "url('/images/vector.png')",
